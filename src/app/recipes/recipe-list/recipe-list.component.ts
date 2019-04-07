10859196418d1419cac69158d786f890facbedc8
Josh <grafton.josh@gmail.com>
2019-04-07 11:53:31 -0600

    Setup structure of recipe components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'Test description', 'http://placekitten.com/200/300')
+    new Recipe('A test recipe', 'Test description', 'http://placekitten.com/200/300'), new Recipe('A test recipe', 'Test description', 'http://placekitten.com/200/300'), new Recipe('A test recipe', 'Test description', 'http://placekitten.com/200/300'), new Recipe('A test recipe', 'Test description', 'http://placekitten.com/200/300')
   ]
 
 
