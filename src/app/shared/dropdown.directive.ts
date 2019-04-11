edb062afb29f7ada969a8b59f5941b93bb498310
Josh <grafton.josh@gmail.com>
2019-04-11 11:33:58 -0600

    formatting changes

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -6,8 +6,11 @@ import { Directive, HostListener, HostBinding } from '@angular/core'
 
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false
-  
+
+
+
   @HostListener('click') toggleOpen(){
     this.isOpen = !this.isOpen
   }
+
 }
